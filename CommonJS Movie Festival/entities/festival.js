f077f0e035fe8301ee14935adddc733542224471
BIT Student <classroom@bgit.rs>
2018-03-12 11:47:11 +0100

    commonJS

---
+++ CommonJS Movie Festival/entities/festival.js
@@ -0,0 +1,40 @@
+function Festival(name) {
+    this.name = name;
+    this.listOfPrograms = [];
+};
+
+/**
+ * Adds program to the list of programs
+ */
+Festival.prototype.addProgram = function (program) {
+    this.listOfPrograms.push(program);
+};
+
+Festival.prototype.getMoviesCount = function () {
+    var programs = this.listOfPrograms;
+    var moviesCount = 0;
+
+    for (var i = 0; i < programs.length; i++) {
+        var program = programs[i];
+        moviesCount += program.listOfMovies.length;
+    }
+
+    return moviesCount;
+};
+
+Festival.prototype.getData = function () {
+    var festivalName = this.name;
+    var programs = this.listOfPrograms;
+    var totalMovieCount = this.getMoviesCount();
+
+    var outputStr = festivalName + " has " + totalMovieCount + " movie titles\n"
+
+    for (var i = 0; i < programs.length; i++) {
+        var program = programs[i];
+        outputStr += "\t" + program.getData();
+    }
+
+    return outputStr;
+};
+
+module.exports = { Festival };
